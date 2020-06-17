3c46fef32f72727d4c452f0087ad202e52fd7ec6
sgoelman <sgoelman@gmail.com>
2020-06-17 11:37:00 +0300

    created get files method

--- src/temp.py
+++ src/temp.py
@@ -9,12 +9,12 @@ def search_for_file_with_ending(contains_a, contains_b):
     for root, dirs, files in os.walk('C:\\'):
         for file in files:
             if file.__contains__(contains_a) or file.__contains__(contains_b):
-                search_list.append(root + '\\' + str(file))
-    return search_list
+                path = root + '\\' + str(file)
+                yield file, path
 
 
-def get_creation_date(path_to_file,file):
-    file_dict={}
+def get_creation_date(path_to_file, file):
+    file_dict = {}
     if platform.system() == 'Windows':
         # file_dict={path_to_file:}
         return os.path.getctime(path_to_file)
@@ -27,11 +27,9 @@ def get_creation_date(path_to_file,file):
             # Linux OS
             return stat.st_mtime
 
+file_dict=[]
+for file, path in search_for_file_with_ending('XT.ec', 'AcGenral'):
+    file_dict.append(file, path)
 
-file_paths_list = search_for_file_with_ending('XT.ec', 'AcGenral')
-creation_date = []
-for path in file_paths_list:
-    creation_date.append(creation_date)
+print('Files:' + str(file_dict))
 
-print('Files:' + str(file_paths_list))
-print('Dates:' + (str(creation_date)))
