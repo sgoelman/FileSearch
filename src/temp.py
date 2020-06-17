666e41e0221df41564b8d813841e2891024ba618
sgoelman <sgoelman@gmail.com>
2020-06-17 11:16:57 +0300

    created get files method

---
+++ src/temp.py
@@ -0,0 +1,33 @@
+import os
+import platform
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def search_for_file_with_ending(contains_a, contains_b):
+    search_list = []
+    for root, dirs, files in os.walk('C:\\'):
+        for file in files:
+            if file.__contains__(contains_a) or file.__contains__(contains_b):
+                search_list.append(root + '\\' + str(file))
+    return search_list
+
+
+def get_creation_date(path_to_file):
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            return stat.st_mtime
+
+
+file_paths_list = search_for_file_with_ending('XT.ec', 'AcGenral')
+creation_date = []
+for path in file_paths_list:
+    creation_date.append(creation_date)
+
+print('Files:' + str(file_paths_list))
+print('Dates:' + (str(creation_date)))
